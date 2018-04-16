87e85c044da267e77fed2830884edc5825733bc7
Loïc Pinsard <muream@gmail.com>
2018-04-16 17:11:04 +0200

    bot can join and leave voice channel

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client()
 
+const commands  = {
+    'ping': message => message.channel.sendMessage('pong'),
+    'join': message => joinChannelFromCommand(message),
+    'leave': message =>  leaveChannel(message)
+}
+
+function joinChannelFromCommand(message) {
+    let channel = message.member.voiceChannel
+    if(channel) {
+        channel.join()
+    } else {
+        message.channel.sendMessage('You need to join a voice channel first.')
+    }
+}
+
+function leaveChannel(message) {
+    let channel = message.member.voiceChannel
+    if(channel) {
+        console.log(typeof channel)
+        channel.leave()
+    } else {
+        message.channel.sendMessage('I cannot leave')
+    }
+}
+
+
 bot.on('message', (message) => {
-    if (message.content == 'ping'){
-        message.channel.sendMessage('pong')
+    if (commands.hasOwnProperty(message.content)) {
+        commands[message.content](message)
     }
 })
 
