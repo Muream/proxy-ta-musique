3dcf6d9933fb0d2413aed0f42d21903aa6bd5578
Loïc Pinsard <muream@gmail.com>
2018-04-16 18:56:22 +0200

    bot can play youtube links

--- index.js
+++ index.js
@@ -1,16 +1,33 @@
 const Discord = require('discord.js')
+const ytdl = require('ytdl-core')
 const bot = new Discord.Client()
 
-const commands  = {
+var connection = null
+
+const commands = {
     'ping': message => message.channel.sendMessage('pong'),
     'join': message => joinChannelFromCommand(message),
-    'leave': message =>  leaveChannel(message)
+    'leave': message => leaveChannel(message),
+    'play': message => playSound(message),
 }
 
-function joinChannelFromCommand(message) {
+async function playSound(message) {
+    let url = getYoutubeURL(message.content)
+    connection.playStream(ytdl(
+        url, { filter: 'audioonly' }
+    ), { volume: 0.1 })
+}
+
+function getYoutubeURL(message) {
+    let url = message.match(/\bhttps?:\/\/\S+/gi)[0]
+    console.log(url)
+    return url
+}
+
+async function joinChannelFromCommand(message) {
     let channel = message.member.voiceChannel
-    if(channel) {
-        channel.join()
+    if (channel) {
+        connection = await channel.join()
     } else {
         message.channel.sendMessage('You need to join a voice channel first.')
     }
@@ -18,9 +35,9 @@ function joinChannelFromCommand(message) {
 
 function leaveChannel(message) {
     let channel = message.member.voiceChannel
-    if(channel) {
-        console.log(typeof channel)
+    if (channel) {
         channel.leave()
+        connection = null
     } else {
         message.channel.sendMessage('I cannot leave')
     }
@@ -28,8 +45,9 @@ function leaveChannel(message) {
 
 
 bot.on('message', (message) => {
-    if (commands.hasOwnProperty(message.content)) {
-        commands[message.content](message)
+    let command = message.content.split(' ')[0]
+    if (commands.hasOwnProperty(command)) {
+        commands[command](message)
     }
 })
 
